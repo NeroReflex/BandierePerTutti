4a8ae0cf9f48988347f3ee77ed7da899d3f0fa5f
Denis Benato <benato.denis96@gmail.com>
2016-07-14 23:24:58 +0200

    Mastergot something important!

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+/*    THE APPLICATION WAS CREATED WITH 'composer create-project'    */
+require __DIR__.'/vendor/autoload.php';
+
+use Gishiki\Gishiki;
+
+//run an instance of the application
+Gishiki::Run();
