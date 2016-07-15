f17533f6e167aa3b72c3deef4b21dece004ae8b1
Denis Benato <benato.denis96@gmail.com>
2016-07-15 18:43:32 +0200

    More security and license disclaimer

--- application/routes.php
+++ application/routes.php
@@ -1,12 +1,31 @@
 <?php
+/*
+	BandierePerTutti set and reset flags!
+    Copyright (C) 2016 Benato Denis
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 /*******************************************************************************
  *                                                                             *
- *              Example router for the Gishiki framework                       *
+ *        BandierePerTutti router for the Gishiki framework                    *
  *                                                                             *
  *******************************************************************************/
 
 //import the router (Route class)
 use Gishiki\Core\Route;
+use Gishiki\Core\Environment;
 use Gishiki\HttpKernel\Request;
 use Gishiki\HttpKernel\Response;
 use Gishiki\Algorithms\Collections\GenericCollection;
@@ -21,10 +40,15 @@ Route::post("/set/{flag}", function (Request &$request, Response &$response, Gen
 		"name" 		=> $arguments['flag'],
 		"status" 	=> "set"
 		]);
-		
-	$flags = json_decode(file_get_contents("flags.json"), true);
-	$flags[$arguments['flag']] = "set";
-	$flags = file_put_contents("flags.json", json_encode($flags, JSON_PARTIAL_OUTPUT_ON_ERROR));
+	
+	if ($request->getDeserializedBody()["tokenID"] == Environment::GetCurrentEnvironment()["tokenID"]) {
+		$flags = json_decode(file_get_contents("flags.json"), true);
+		$flags[$arguments['flag']] = "set";
+		$flags = file_put_contents("flags.json", json_encode($flags, JSON_PARTIAL_OUTPUT_ON_ERROR));
+	} else {
+		$result["status"] => "untouched";
+		$result["error"] =>  "bad TokenID";
+	}
 	
 	//send the response to the client
     $response->setSerializedBody($result);
@@ -38,11 +62,15 @@ Route::post("/clear/{flag}", function (Request &$request, Response &$response, G
 		"name" 		=> $arguments['flag'],
 		"status" 	=> "clear"
 		]);
-		
-	$flags = json_decode(file_get_contents("flags.json"), true);
-	$flags[$arguments['flag']] = "clear";
-	$flags = file_put_contents("flags.json", json_encode($flags, JSON_PARTIAL_OUTPUT_ON_ERROR));
 	
+	if ($request->getDeserializedBody()["tokenID"] == Environment::GetCurrentEnvironment()["tokenID"]) {
+		$flags = json_decode(file_get_contents("flags.json"), true);
+		$flags[$arguments['flag']] = "clear";
+		$flags = file_put_contents("flags.json", json_encode($flags, JSON_PARTIAL_OUTPUT_ON_ERROR));
+	} else {
+		$result["status"] => "untouched";
+		$result["error"] =>  "bad TokenID";
+	}
 	
 	//send the response to the client
     $response->setSerializedBody($result);
