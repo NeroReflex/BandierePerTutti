7e7a4db8d61878f894a8efedab165794dca9d85e
Denis Benato <benato.denis96@gmail.com>
2016-08-09 16:09:30 +0200

    Solved double-init problem

--- index.php
+++ index.php
@@ -2,7 +2,3 @@
 /*    THE APPLICATION WAS CREATED WITH 'composer create-project'    */
 require __DIR__.'/vendor/autoload.php';
 
-use Gishiki\Gishiki;
-
-//run an instance of the application
-Gishiki::Run();
